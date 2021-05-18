39c3cefd17a95edf84f1dcd2b38a9525d08fa4d3
gabrimatic <h.yusefpour@gmail.com>
2021-05-18 14:33:44 +0430

    Updated to version 1.0.1;

--- test/restart_app_test.dart
+++ test/restart_app_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:restart_app/restart.dart';
+import 'package:restart_app/restart_app.dart';
 
 void main() {
   const MethodChannel channel = MethodChannel('restart');
